1c55b659a8938b347ac28b5324abb981bd7c2ab9
Mariam <mariansaad585@gmail.com>
2023-03-11 15:17:09 +0200

    curl

---
+++ Day1/webservices1/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("__WEATHER_URL", "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid=ba1daa89d0f3f9980562528b3b3e6115");
+define("__CITIES_FILE", "resources/city.list.json");
+define("__API_key", "&appid=7564fe50b8f254e686c13ea65ff3e54b");
+define("__API", "https://api.openweathermap.org/data/2.5/weather?id=");
+
+?>
